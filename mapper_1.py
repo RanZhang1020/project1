77983cacbda8804cc480f3e8d0721d7cc87bf266
RanZhang1020 <39968768+RanZhang1020@users.noreply.github.com>
2019-03-29 00:14:26 +0800

    Update mapper_1.py

--- mapper_1.py
+++ mapper_1.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 import sys
+import string
+import pandas as pd
+
+##df = pd.read_csv(sys.stdin)
+##nba = df.replace(['made','missed'],[0,1])
 for line in sys.stdin:
     data = line.strip().split(',')
     if data[3] == 'missed':
