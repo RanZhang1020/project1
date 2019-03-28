601113be78900d088dea34a17a992e8bf22c0312
RanZhang1020 <39968768+RanZhang1020@users.noreply.github.com>
2019-03-28 12:46:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /nba/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /nba/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /nba/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-python/nba/test1.csv /nba/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-python/project1/test1.csv /project1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../../mapreduce-test-python/nba/mapper_1.py -mapper ../../mapreduce-test-python/nba/mapper_1.py \
--file ../../mapreduce-test-python/nba/reducer_1.py -reducer ../../mapreduce-test-python/nba/reducer_1.py \
--input /nba/input/* -output /nba/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /nba/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /nba/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /nba/output/
+-file ../../mapreduce-test-python/project1/mapper_1.py -mapper ../../mapreduce-test-python/projecr1/mapper_1.py \
+-file ../../mapreduce-test-python/project1/reducer_1.py -reducer ../../mapreduce-test-python/project1/reducer_1.py \
+-input /project1/input/* -output /project1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /project1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /project/output/
 ../../stop.sh
